40c8437d632bedb8355c12d103c34f4883242510
kokoda-yo <angus.li2000@gmail.com>
2021-01-05 20:25:19 +0800

    Added functions that return random values in a certain range; Add Functions to modify text more easily; Started with the details of the Dice state

--- main.cpp
+++ main.cpp
@@ -2,11 +2,11 @@
 #include <iostream>
 #include <random>
 #include <string>
+#include <ctime>
 
 enum State { // 這告訴我們現在在哪個狀況下 然後main 就會跑相對應的程式
     MENU, 
-    NAME_1,
-    NAME_2,
+    NAME,
     DICE, // when the first player rolls the dice
     CITY, // should be <location type 1> state_1
     MIAO_LI,
@@ -22,7 +22,8 @@ const int kWindowHeight = 1000;
 // Game Setting Consts
 const int kPlayerNum = 2;
 const int kLocationIndexNum = 28;
-const int kStartMoney = 2000;
+const int kMoneyEachRound = 200000;
+const int kStartMoney = 20000000;
 const int kBribeNum = 5;
 const int kStayNum = 3;
 
@@ -40,11 +41,24 @@ const int kSpeechGetMoneyMin = 100000;
 const int kSpeechGetMoneyMax = 300000;
 const int kSpeechGetMoneyRange = kSpeechGetMoneyMax - kSpeechGetMoneyMin;
 
+// Random Functions:
+int RandomDice() {
+  return rand() % kDiceRange;
+}
+
+int RandomBaiPiaoVote() {
+  return rand() % kBaiPiaoVoteRange;
+}
+
+int RandomSpeechGetMoney() {
+  return rand() % kSpeechGetMoneyRange;
+}
 
 
 class Player {
   private:
   std::string name_;
+  int player_index_;
   int location_index_;
   int money_;
   int votes_;
@@ -54,8 +68,9 @@ class Player {
   public:
   // Constructors and Destructors
   // - no Destructors, because no pointers exists.
-  Player(std::string name) {
+  Player(std::string name, int player_index) {
     name_ = name;
+    player_index_ = player_index;
     location_index_ = 0;
     money_ = kStartMoney;
     votes_ = 0;
@@ -64,6 +79,12 @@ class Player {
   }
 
   // Accessors and Mutators
+  // Player Index
+  int get_player_index() {
+    return player_index_;
+  }
+
+  // Location Index
   int get_location_index() {
     return location_index_;
   }
@@ -104,17 +125,13 @@ class Player {
     return (stay_day_ > 0) ? true : false;
   }
 
-  
-    
-  
-
 };
 
 
 class Location {
   private:
   const std::string name_;
-  int vote_[kPlayerNum];
+  int vote_[kPlayerNum]; // 百分比
   int current_winner_;
 
   public:
@@ -132,22 +149,36 @@ class Location {
     return current_winner_;
   }
 
-  // Functions
-  void BaiPiao(int player_num, int random_number) { // assuming only 2 players
-    std::uniform_int_distribution<int> bai_piao_range(kBaiPiaoVoteMin, kBaiPiaoVoteMax);
-    //player(player_num).LostMoney
-    int delta_vote = bai_piao_range(random_number);
-    vote_[player_num] += delta_vote;
-    vote_[std::abs(1 - player_num)] -= delta_vote;
+  // Member Functions
+  void BaiPiao(Player &player) { // assuming only 2 players
+    int delta_vote = RandomBaiPiaoVote();
+    vote_[player.get_player_index()] += delta_vote;
+    vote_[std::abs(1 - player.get_player_index())] -= delta_vote;
+    player.update_money(-kBaiPiaoSpendMoney);
   }
 
-  void Speech(int player_num, int random_number) {
-    std::uniform_int_distribution<int> speech_money_range(kSpeechGetMoneyMin, kSpeechGetMoneyMax);
-    int delta_money = speech_money_range(random_number);
-    // add money for player
+  void Speech(Player &player) {
+    int delta_money = RandomSpeechGetMoney();
+    player.update_money(delta_money);
   }
 
 };
+
+void BuildText(sf::Text &text, const sf::Font &font, const sf::String &content, unsigned int size,
+               const sf::Color &color, sf::Uint32 style, float x, float y ) {
+  text.setFont(font);
+  text.setString(content);
+  text.setCharacterSize(size);
+  text.setFillColor(color);
+  text.setStyle(style);
+  text.setPosition(x, y);
+} 
+
+void ChangeDiceText(sf::Text &text, const int dice_number) {
+  std::string dice_string = "You rolled " + std::to_string(dice_number) + " steps";
+  text.setString(dice_string);
+}
+
 
 
 int main (int argc, char** argv) {
@@ -159,41 +190,30 @@ int main (int argc, char** argv) {
   sf::Font big_font;
   big_font.loadFromFile("fonts/HanaleiFill-Regular.ttf");
 
-  // Menu words
+  // Menu Text
   sf::Text menu_text;
-  menu_text.setFont(big_font);
-  menu_text.setString("menu");
-  menu_text.setCharacterSize(50);
-  menu_text.setFillColor(sf::Color::Blue);
-  menu_text.setStyle(sf::Text::Bold);
-  //menu setposition
+  BuildText(menu_text, big_font, "MENU", 50, sf::Color::White, sf::Text::Bold, 500, 500);
 
   sf::Text menu_sentence;
-  menu_sentence.setFont(big_font);
-  menu_sentence.setString("press space to continue");
-  menu_sentence.setCharacterSize(25);
-  menu_sentence.setFillColor(sf::Color::Magenta);
-  menu_sentence.setStyle(sf::Text::Italic);
-  menu_sentence.setPosition(0, 100); // 暫時
+  BuildText(menu_sentence, big_font, "press space to continue", 25, sf::Color::Magenta, sf::Text::Italic, 500, 700);
 
 // Game setup
-  // Dice setup // 這個失敗了 我到時候改 rand() 版本
-  std::uniform_int_distribution<int> random_dice_range(kDiceMin, kDiceMax);
-  std::random_device rd;
-  std::mt19937 random_number(rd());
-  
-  int dice_value = 2;
-  sf::Text dice_sentence;
-  dice_sentence.setFont(big_font);
-  std::string dice_string = "You rolled " + std::to_string(dice_value) + " steps";
-  dice_sentence.setString(dice_string);
+  // Dice Randomizer
+  srand(time(0));
+  int dice_value = 0;
 
-// Sprite Setup
-  //測試貓 沒啥特別意義 就測試
-  sf::Texture cat_texture;
-  cat_texture.loadFromFile("images/cat.png");
-  sf::Sprite cat_sprite;
-  cat_sprite.setTexture(cat_texture);
+  // Dice Prompt Text
+  sf::Text dice_prompt_text;
+  BuildText(dice_prompt_text, big_font, "Press Space to roll the dice!", 50, sf::Color::Blue, sf::Text::Bold, 500, 250);
+
+  // Dice Text
+  sf::Text dice_text;
+  BuildText(dice_text, big_font, "", 25, sf::Color::White, sf::Text::Regular, 500, 500);
+
+  // Tell Location Text
+  sf::Text tell_location_text;
+  BuildText(tell_location_text, big_font, "", 25, sf::Color::White, sf::Text::Regular, 500, 500);
+// BackGround Setup
 
   //暫時地圖
   sf::Texture board_texture;
@@ -203,6 +223,7 @@ int main (int argc, char** argv) {
 
 
   // player1跟2 座標
+
 // Location Setup
   std::string list_of_location_names[kLocationIndexNum] = {
     "總統府",
@@ -240,10 +261,14 @@ int main (int argc, char** argv) {
   }
 
 // Player Setup
+  std::string list_of_player_names[kPlayerNum] = {"one", "two"};
   Player* players[kPlayerNum];
+  for (int i = 0; i < kPlayerNum; i++) {
+    players[i] = new Player(list_of_player_names[i], i); // 在沒有做輸入player1 2的名字前 先暫時這樣
+  }
 
 
-  
+  int current_id = 0;
   state = MENU;
   int current_round = 0;
   while (render_window.isOpen()) {
@@ -259,6 +284,7 @@ int main (int argc, char** argv) {
               break;
             case sf::Event::EventType::KeyPressed:
               if (ev.key.code == sf::Keyboard::Space) {
+
                 state = DICE;
               }
               break;
@@ -271,6 +297,37 @@ int main (int argc, char** argv) {
         break;
 
       case DICE:
+        while (render_window.pollEvent(ev)) {
+          switch (ev.type) {
+            case sf::Event::EventType::Closed:
+              render_window.close();
+              break;
+            case sf::Event::EventType::KeyPressed:
+              if (ev.key.code == sf::Keyboard::Space) {
+                // add 20,000
+                players[current_id]->update_money(kMoneyEachRound);
+
+                // throw the dice, update location
+                int old_location_index = players[current_id]->get_location_index();
+                int dice_value = RandomDice();
+                players[current_id]->update_location_index(dice_value);
+                int new_location_index = players[current_id]->get_location_index();
+
+                // check if went past start(總統府). If so, add money
+                if (new_location_index - old_location_index < 0) {
+
+                }
+
+                if (players[current_id]->get_location_index() == 7) {
+
+                }
+              }
+              break;
+          }
+        }
+        render_window.clear(sf::Color::Black);
+        render_window.draw(dice_prompt_text);
+        render_window.display();
         break;
             
       case CITY:
